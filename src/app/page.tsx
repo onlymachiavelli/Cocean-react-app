8408e378ec4d45f3b99d7fc2ec41d57e098982ff
omgomarjomaa <omarjomaa333@gmail.com>
2024-04-21 14:58:37 -0700

    Commit message

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import {} from "react"
+import { Button } from "@nextui-org/react"
+const App = () => {
+  return (
+    <>
+      <Button color="danger">Kill this bitch</Button>
+    </>
+  )
+}
+
+export default App
